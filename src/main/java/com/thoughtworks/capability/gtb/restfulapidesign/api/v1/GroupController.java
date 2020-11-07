fcf2778ffcdfc7e1f445ee48d866e0d0c05d99a9
flappyBug <tdkgn@outlook.com>
2020-11-07 23:44:00 +0800

    implement shuffle groups api

---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/v1/GroupController.java
@@ -0,0 +1,26 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.api.v1;
+
+import com.thoughtworks.capability.gtb.restfulapidesign.domain.Group;
+import com.thoughtworks.capability.gtb.restfulapidesign.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/groups")
+public class GroupController {
+    private final GroupService groupService;
+
+    public GroupController(GroupService groupService) {
+        this.groupService = groupService;
+    }
+
+
+    @PutMapping("/shuffle")
+    List<Group> shuffleGroups() {
+        return groupService.shuffleGroups();
+    }
+}
---
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/GroupService.java
@@ -0,0 +1,46 @@
+package com.thoughtworks.capability.gtb.restfulapidesign.service;
+
+import com.thoughtworks.capability.gtb.restfulapidesign.domain.Group;
+import com.thoughtworks.capability.gtb.restfulapidesign.domain.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@Service
+public class GroupService {
+    private final List<Group> groups;
+    private final StudentService studentService;
+    private Random random = new Random();
+    private final Integer TOTAL_GROUPS = 6;
+
+    public GroupService(StudentService studentService) {
+        this.studentService = studentService;
+        groups = IntStream.rangeClosed(1, TOTAL_GROUPS)
+                .mapToObj(id -> new Group(id, "Team " + id, new ArrayList<>(), null)).collect(Collectors.toList());
+    }
+
+    private Student randomPop(List<Student> students) {
+        return students.remove(random.nextInt(students.size()));
+    }
+
+    private void clearGroups() {
+        groups.forEach(group -> group.getStudents().clear());
+    }
+
+    public List<Group> shuffleGroups() {
+        clearGroups();
+        List<Student> students = studentService.queryStudents(null);
+        int groupIndex = 0;
+        while (!students.isEmpty()) {
+            groups.get(groupIndex).getStudents().add(randomPop(students));
+            groupIndex = (groupIndex + 1) % TOTAL_GROUPS;
+        }
+        return groups;
+    }
+}
