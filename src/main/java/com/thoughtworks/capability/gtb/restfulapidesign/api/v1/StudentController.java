e8fa72cfa79abb64d807bb843cf862cbcf078a56
flappyBug <tdkgn@outlook.com>
2020-11-07 21:24:22 +0800

    implement remove student api

--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/v1/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/api/v1/StudentController.java
@@ -3,6 +3,7 @@ package com.thoughtworks.capability.gtb.restfulapidesign.api.v1;
 import com.thoughtworks.capability.gtb.restfulapidesign.domain.Student;
 import com.thoughtworks.capability.gtb.restfulapidesign.service.StudentService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +18,17 @@ public class StudentController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    void registerStudent(Student student) {
+    void registerStudent(@RequestBody Student student) {
         studentService.registerStudent(student);
     }
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> removeStudent(@PathVariable Integer id) {
+
+        if (studentService.removeStudent(id) == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
 }
--- src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/StudentService.java
+++ src/main/java/com/thoughtworks/capability/gtb/restfulapidesign/service/StudentService.java
@@ -3,18 +3,20 @@ package com.thoughtworks.capability.gtb.restfulapidesign.service;
 import com.thoughtworks.capability.gtb.restfulapidesign.domain.Student;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PrimitiveIterator;
+import java.util.*;
 import java.util.stream.IntStream;
 
 @Service
 public class StudentService {
     PrimitiveIterator.OfInt idGenerator = IntStream.iterate(0, i -> i + 1).iterator();
-    private List<Student> students = new ArrayList<>();
+    private final Map<Integer, Student> students = new HashMap<>();
 
     public void registerStudent(Student student) {
         student.setId(idGenerator.next());
-        students.add(student);
+        students.put(student.getId(), student);
+    }
+
+    public Student removeStudent(Integer id) {
+        return students.remove(id);
     }
 }
